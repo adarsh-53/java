e67a4dc38a8bc2406b96fb0fcfc8c9b433a0aa3c
adarsh <106797645+adarsh-53@users.noreply.github.com>
2022-06-23 12:42:00 +0530

    sub file created

--- sum.java
+++ sum.java
@@ -9,3 +9,15 @@ class sum
     System.out.println("sum of 2 numbers is"+c);
   }
 }
+
+
+class sub
+{
+  public static void main (String args[])
+  {
+    int a=10;
+    int b=5;
+    int c=a-b;
+    System.out.println("difference of 2 number is"+c);
+  }
+}
